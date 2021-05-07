a452740d4da30855237a2162d3d55c81fb027f96
Lingye Zhuang <l1zhuang@ucsd.edu>
2021-05-07 00:16:36 -0700

    ACM_website case study √

--- js/casestudy.js
+++ js/casestudy.js
@@ -1,14 +1,26 @@
 /* Max-min img. */
-var img = document.querySelectorAll(".mined_img");
-console.log (img);
-var modal = document.querySelector(".modal");
-var modal_img = document.querySelector(".modal_img");
-img.forEach(item => {
+var img_desktop = document.querySelectorAll(".mined_desktop");
+console.log(img_desktop);
+var img_mobile = document.querySelectorAll(".mined_mobile");
+var modal_desktop = document.querySelector(".modal_desktop");
+var modal_img_desktop = document.querySelector(".modal_img_desktop");
+var modal_mobile = document.querySelector(".modal_mobile");
+var modal_img_mobile = document.querySelector(".modal_img_mobile");
+img_desktop.forEach(item => {
 	item.addEventListener('click', event => {  // open modal img
-		modal_img.src = item.src;
-		modal.style.display = "block";
+		modal_img_desktop.src = item.src;
+		modal_desktop.style.display = "block";
 	})
 });
-modal.addEventListener('click', () => {  // minimize img
-	modal.style.display = "none";
+modal_desktop.addEventListener('click', () => {  // minimize img
+	modal_desktop.style.display = "none";
+});
+img_mobile.forEach(item => {
+	item.addEventListener('click', event => {
+		modal_img_mobile.src = item.src;
+		modal_mobile.style.display = "block";
+	})
+});
+modal_mobile.addEventListener('click', () => {  // minimize img
+	modal_mobile.style.display = "none";
 });
